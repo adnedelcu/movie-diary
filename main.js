b6d21d0b821018581d53048c8d27ed2e047621ed
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-07-18 19:50:17 +0200

    Added logic for calling API with API KEY

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import { API_KEY } from './config.js';
 
 console.log(API_KEY);
+
+const url = 'https://api.themoviedb.org/3/movie/popular';
+const headers = {
+    'Authorization': `Bearer ${API_KEY}`,
+}
+
+fetch(url, { headers })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
