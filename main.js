47b4a6b18c13930e922f1b0cd6d561cd9ea3850b
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-07-22 20:11:42 +0200

    Added pagination to main page

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import { API_KEY } from './config.js';
 
-const url = 'https://api.themoviedb.org/3/movie/popular';
+const searchParams = new URLSearchParams(window.location.search);
+
+let maxPages = 1;
+const currentPage = parseInt(searchParams.get('page') || '1');
+
+const url = `https://api.themoviedb.org/3/movie/popular?page=${currentPage}`;
 const headers = {
     'Authorization': `Bearer ${API_KEY}`,
 }
@@ -92,15 +97,20 @@ const genres = [
 const template = document.getElementById('movie-card');
 const resultDiv = document.getElementById('grid-search-results');
 
+const currentPageEl = document.getElementById('currentPage');
+const maxPagesEl = document.getElementById('maxPages');
+currentPageEl.innerText = currentPage;
 
 // Popular Movies List
 fetch(url, { headers })
     .then((response) => {
         if(!response.ok) throw new Error ('Sorry. Someting went wrong.');
         return response.json()})
-    
+
     .then((data => {
         console.log(data);
+        maxPages = data.total_pages;
+        updatePagination();
         data.results.forEach(movie => {
             const templateDiv = document.createElement('div');
             templateDiv.classList.add('bg-gray-800', 'rounded-lg', 'overflow-hidden', 'shadow-lg');
@@ -108,13 +118,13 @@ fetch(url, { headers })
             const imgUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "https://i.pinimg.com/originals/cd/23/c7/cd23c7a8d049049fd1b0ef281f0300cb.jpg";
             poster.src = imgUrl;
             poster.classList.add('movie-poster');
-            
+
             const titleDiv = document.createElement('div');
             titleDiv.classList.add('p-4');
             const movieTitle = document.createElement('h3');
             movieTitle.textContent = movie.original_title.toUpperCase();
             movieTitle.classList.add('text-xl', 'font-semibold', 'mb-2', 'mt-1', 'ml-1', 'text-center', 'truncate')
-            
+
             const descriptionP = document.createElement('p');
             descriptionP.classList.add('text-gray-400', 'mb-4', 'text-center');
             const genreId = movie.genre_ids[0];
@@ -134,16 +144,16 @@ fetch(url, { headers })
             templateDiv.appendChild(descriptionP);
             templateDiv.appendChild(diaryBtn);
             diaryBtn.appendChild(btnSpan);
-            
-            
+
+
 
 
             //console.log(movie.original_title.toUpperCase());
             //console.log(movie.release_date.slice(0,4));
             //console.log(genre.name);
-            //console.log(imgUrl);    
+            //console.log(imgUrl);
         });
-        
+
         }))
     .catch(error => console.error(error));
 
@@ -185,7 +195,7 @@ function displayResults(data) {
             const movieTitle = document.createElement('h3');
             movieTitle.textContent = movie.original_title.toUpperCase();
             movieTitle.classList.add('text-xl', 'font-semibold', 'mb-2', 'mt-1', 'ml-1', 'text-center', 'truncate')
-            
+
             const descriptionP = document.createElement('p');
             descriptionP.classList.add('text-gray-400', 'mb-4', 'text-center');
             const genreId = movie.genre_ids[0];
@@ -197,8 +207,8 @@ function displayResults(data) {
             const btnSpan = document.createElement('span');
             btnSpan.classList.add('flex', 'items-center', 'justify-center', 'gradient-border-span');
             btnSpan.innerHTML = "<i class='fas fa-bookmark mr-2'></i>Add to Diary";
-            
-            
+
+
             templateDiv.appendChild(poster);
             templateDiv.appendChild(movieTitle);
             poster.alt = movieTitle.original_title;
@@ -206,9 +216,9 @@ function displayResults(data) {
             templateDiv.appendChild(diaryBtn);
             diaryBtn.appendChild(btnSpan);
             resultDiv.appendChild(templateDiv);
-            
+
         });
-    }     
+    }
     else {
         resultDiv.textContent = "No results found.";
     }
@@ -221,5 +231,55 @@ function displayResults(data) {
         clearTimeout(timeout);
         timeout = setTimeout(() => func.apply(this, args), wait);
     };
-} 
+}
+
+function updatePagination() {
+    maxPagesEl.innerText = maxPages;
+    const navEl = document.getElementById('pagination');
+
+    const firstPageEl = document.createElement('a');
+    firstPageEl.classList.add('relative', 'inline-flex', 'items-center', 'rounded-l-md', 'px-2', 'py-2', 'text-gray-400', 'ring-1', 'ring-inset', 'ring-gray-300', 'hover:bg-gray-50', 'focus:z-20', 'focus:outline-offset-0');
+    firstPageEl.innerHTML = '<span class="sr-only">First</span>&lt;&lt;';
+    // firstPageEl.href = 'nohref';
+    if (currentPage > 1) {
+        firstPageEl.href = `${window.location.origin}${window.location.pathname}?page=1`;
+    }
+    navEl.appendChild(firstPageEl);
+
+    const prevPageEl = document.createElement('a');
+    prevPageEl.classList.add('relative', 'inline-flex', 'items-center', 'px-2', 'py-2', 'text-gray-400', 'ring-1', 'ring-inset', 'ring-gray-300', 'hover:bg-gray-50', 'focus:z-20', 'focus:outline-offset-0');
+    prevPageEl.innerHTML = '<span class="sr-only">Previous</span>&lt;';
+    // prevPageEl.href = 'nohref';
+    if (currentPage > 1) {
+        prevPageEl.href = `${window.location.origin}${window.location.pathname}?page=${currentPage-1}`;
+    }
+
+    navEl.appendChild(prevPageEl);
+
+    const pageEl = document.createElement('a');
+    pageEl.classList.add('relative', 'z-10', 'inline-flex', 'items-center', 'bg-indigo-600', 'px-4', 'py-2', 'text-sm', 'font-semibold', 'text-white', 'focus:z-20', 'focus-visible:outline', 'focus-visible:outline-2', 'focus-visible:outline-offset-2', 'focus-visible:outline-indigo-600');
+    pageEl.ariaCurrent = 'page';
+    pageEl.innerHTML = currentPage;
+    navEl.appendChild(pageEl);
+
+    const nextPageEl = document.createElement('a');
+    nextPageEl.classList.add('relative', 'inline-flex', 'items-center', 'px-2', 'py-2', 'text-gray-400', 'ring-1', 'ring-inset', 'ring-gray-300', 'hover:bg-gray-50', 'focus:z-20', 'focus:outline-offset-0');
+    nextPageEl.innerHTML = '<span class="sr-only">Next</span>&gt;';
+    // nextPageEl.href = 'nohref';
+    if (currentPage < maxPages) {
+        nextPageEl.href = `${window.location.origin}${window.location.pathname}?page=${currentPage+1}`;
+    }
+
+    navEl.appendChild(nextPageEl);
+
+    const lastPageEl = document.createElement('a');
+    lastPageEl.classList.add('relative', 'inline-flex', 'items-center', 'rounded-r-md', 'px-2', 'py-2', 'text-gray-400', 'ring-1', 'ring-inset', 'ring-gray-300', 'hover:bg-gray-50', 'focus:z-20', 'focus:outline-offset-0');
+    lastPageEl.innerHTML = '<span class="sr-only">Last</span>&gt;&gt;';
+    // lastPageEl.href = 'nohref';
+    if (currentPage < maxPages) {
+        lastPageEl.href = `${window.location.origin}${window.location.pathname}?page=${maxPages}`;
+    }
+
+    navEl.appendChild(lastPageEl);
+}
 
