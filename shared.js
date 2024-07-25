59e040098f30c5ff62230acebf6df6e782d2ef46
timataliatov <ttaliatov@gmail.com>
2024-07-25 14:10:46 +0200

    Search Grid fix

--- shared.js
+++ shared.js
@@ -122,7 +122,7 @@ export const genres = [
   { id: 35, name: "Comedy" }, { id: 80, name: "Crime" }, { id: 99, name: "Documentary" },
   { id: 18, name: "Drama" }, { id: 10751, name: "Family" }, { id: 14, name: "Fantasy" },
   { id: 36, name: "History" }, { id: 27, name: "Horror" }, { id: 10402, name: "Music" },
-  { id: 9648, name: "Mystery" }, { id: 10749, name: "Romance" }, { id: 878, name: "Science Fiction" },
+  { id: 9648, name: "Mystery" }, { id: 10749, name: "Romance" }, { id: 878, name: "Sci-Fi" },
   { id: 10770, name: "TV Movie" }, { id: 53, name: "Thriller" }, { id: 10752, name: "War" },
   { id: 37, name: "Western" }
 ];
@@ -149,7 +149,7 @@ export const createMovieCard = (movie, isDiary = false) => {
   const descriptionP = document.createElement('p');
   descriptionP.classList.add('text-gray-400', 'mb-4', 'text-center');
   const genre = genres.find(x => x.id === movie.genre_ids[0]);
-  descriptionP.textContent = `${movie.release_date ? movie.release_date.slice(0, 4) : 'Unknown Year'} | ${genre?.name || 'Unknown Genre'}`;
+  descriptionP.textContent = `${movie.release_date ? movie.release_date.slice(0, 4) : 'Unknown'} | ${genre?.name || 'Unknown'}`;
 
   // create a container for buttons
   const buttonContainer = document.createElement('div');
